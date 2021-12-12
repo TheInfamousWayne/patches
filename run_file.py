95217f77457c5a378db471876a2d073f6e208f37
Vaibhav Agrawal <vagrawal@tuebingen.mpg.de>
2021-12-12 20:23:56 +0100

    auc vs dict size

--- run_file.py
+++ run_file.py
@@ -4,7 +4,7 @@ import os
 def auc_vs_dictionary_size():
     dict_size = [4096, 2048, 1024, 512, 256, 128, 64, 32, 16, 8]
     for d in dict_size:
-        os.system(f'python auc_kneighbors.py --n_channel_convolution {d} --batchsize 128 --dataset DTD --stride_avg_pooling 3 --spatialsize_avg_pooling 5 --finalsize_avg_pooling 6 --lr_schedule "{0:3e-3,50:3e-4,75:3e-5}" --nepochs 80 --optimizer SGD --bottleneck_dim 128 --padding_mode reflect --kneighbors_fraction 0.4 --convolutional_classifier 6 --whitening_reg 1e-3 --sgd_momentum 0.9 --batch_norm --save_model --save_best_model')
+        os.system('python auc_kneighbors.py --n_channel_convolution %s --batchsize 128 --dataset DTD --stride_avg_pooling 3 --spatialsize_avg_pooling 5 --finalsize_avg_pooling 6 --lr_schedule "{0:3e-3,50:3e-4,75:3e-5}" --nepochs 80 --optimizer SGD --bottleneck_dim 128 --padding_mode reflect --kneighbors_fraction 0.4 --convolutional_classifier 6 --whitening_reg 1e-3 --sgd_momentum 0.9 --batch_norm --save_model --save_best_model' % d)
 
 if __name__ == "__main__":
     auc_vs_dictionary_size()
